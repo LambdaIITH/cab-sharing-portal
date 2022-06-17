58e42c946d80958e0b25b3c3e43efd11bb764748
Vikhyath-vec <vikhyath.kothamasu@gmail.com>
2022-06-16 20:10:22 -0500

    added new booking

--- backend/main.py
+++ backend/main.py
@@ -24,5 +24,15 @@ async def new_user(info : Request):
 @app.post("/cabbooking")
 async def cab_booking(info: Request):
     details = await info.json()
-    # Update cabbooking table, traveller
-    # queries.cab_booking(conn,)
+    email = "cs20btech11056@iith.ac.in"
+    from_id = queries.get_loc_id(conn, place=details['from'])
+    to_id = queries.get_loc_id(conn, place=details['to'])
+    from_id = int(from_id[0])
+    to_id = int(to_id[0])
+    booking_id = queries.cab_booking(conn, date=details['date'], start_time=details['start_time'], 
+                                    end_time=details['end_time'], comments=details['comments'],
+                                    capacity=details['capacity'], from_loc=from_id, to_loc=to_id)
+    
+    queries.add_traveller(conn, id=booking_id, user_email=email)
+
+    conn.commit()
