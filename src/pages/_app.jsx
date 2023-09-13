9d08858317feba502a10a4b5c38d8fc97ed55374
kst164 <kartheek@tammana.org>
2023-09-14 01:36:49 +0530

    React, not Next (in another place)

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,7 +3,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}>
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
       <div data-theme="halloween" className="">
         <Component {...pageProps} />
       </div>
