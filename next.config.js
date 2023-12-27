78352114e091126c7b3db62afd5336234e2e5ebd
Jaswanth Beere <jaswanthbeere@gmail.com>
2023-12-27 10:31:21 +0530

    add required files for PWA

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
+const runtimeCaching = require("next-pwa/cache");
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+});
+
+module.exports = withPWA({
+  reactStrictMode: false,
+});
+
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+// module.exports = nextConfig
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,10 +1,32 @@
 import "../styles/globals.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import Head from "next/head";
 import { ToastContainer } from "react-toastify";
 
 function MyApp({ Component, pageProps }) {
   return (
     <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}>
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
+
+        <meta name="pwa-demo" content="pwa-demo" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="pwa-demo" />
+        <meta name="description" content="pwa-demo" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-TileColor" content="#2B5797" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#000000" />
+
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
       <div data-theme="halloween" className="">
         <Component {...pageProps} />
         <ToastContainer limit={3} />
